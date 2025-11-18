4ba948062b73e11a04b495529afcd0c7ecd68049
alokr2v <alokr2v@gmail.com>
2025-11-18 12:25:29 +0000

    commit config done

---
+++ Class_1/03_Operators.js
@@ -0,0 +1,29 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+/*
+When compair anything must be remember the datatype should be same.
+*/
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+
+// Comparisons convert null to a number treating it as 0. That's why "null>=0" and "null<=0" is true.
+
+// console.log(undefined == 0 )
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+
+// Strict Check (===)
+
+console.log("2" == 2)
+console.log("2" === 2)
