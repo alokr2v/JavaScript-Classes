b3fd2a4e61763357309918b900c4f9d1f18b17e5
alokr2v <alokr2v@gmail.com>
2025-11-18 10:23:02 +0000

    commit config done

---
+++ Class_1/01_variable.js
@@ -0,0 +1,14 @@
+const accountID = 123456
+let accountEmail = "Alok@google.com"
+var accountPassword = "987620"
+accountCity = "Delhi"
+let accountState;
+
+// accountID = 234
+accountEmail = "raj@google.com"
+accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.table({"accountID" : accountID,
+     "accountEmail" : accountEmail,"accountPassword" : accountPassword,
+     "accountCity" : accountCity,"accountState" : accountState})
